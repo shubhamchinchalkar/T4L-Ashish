39182d79a346ffaff44a8de8024fe5cfdeb5335f
Ashish082025 <ashish@i4lgroup.co.uk>
2025-12-11 18:27:15 +0530

    Updates

--- assets/snow-inject.js
+++ assets/snow-inject.js
@@ -1,14 +1,11 @@
-/* assets/snow-inject.js */
+/* Full-width snow overlay injection */
 
 (function(){
 
-  // Your CDN Snow Image (no need to upload to assets)
   var SNOW_IMAGE = "https://cdn.shopify.com/s/files/1/0250/6198/2261/files/Untitled-4.png?v=1765455277";
 
-  // Possible search bar containers used by themes
-  var candidateSelectors = [
+  var selectors = [
     'form[action="/search"]',
-    '#SearchForm',
     '.search-bar',
     '.site-header__search',
     '.header__search',
@@ -17,45 +14,40 @@
   ];
 
   function createOverlay(){
-    var wrap = document.createElement('div');
-    wrap.className = 'snow-overlay-container';
+    var wrap = document.createElement("div");
+    wrap.className = "search-snow-overlay";
 
-    var img = document.createElement('img');
+    var img = document.createElement("img");
     img.src = SNOW_IMAGE;
     img.alt = "";
-    img.setAttribute('aria-hidden', 'true');
+    img.setAttribute("aria-hidden", "true");
 
     wrap.appendChild(img);
     return wrap;
   }
 
-  function insertOverlay(target){
-    var style = window.getComputedStyle(target);
-    if (style.position === 'static'){
-      target.style.position = 'relative';
+  function applySnow(inputBox){
+    var style = window.getComputedStyle(inputBox);
+    if (style.position === "static") {
+      inputBox.style.position = "relative";
     }
 
-    target.appendChild(createOverlay());
+    inputBox.appendChild(createOverlay());
   }
 
-  function tryInsert(){
-    for (var i=0; i<candidateSelectors.length; i++){
-      var el = document.querySelector(candidateSelectors[i]);
-      if (el){
-        insertOverlay(el);
-        return true;
-      }
+  function findTarget(){
+    for (var i = 0; i < selectors.length; i++){
+      var el = document.querySelector(selectors[i]);
+      if (el) return el;
     }
-    return false;
+    return null;
   }
 
   document.addEventListener("DOMContentLoaded", function(){
     setTimeout(function(){
-      if (!tryInsert()){
-        var header = document.querySelector("header");
-        if (header){ insertOverlay(header); }
-      }
-    }, 200);
+      var target = findTarget();
+      if (target) applySnow(target);
+    }, 150);
   });
 
 })();
